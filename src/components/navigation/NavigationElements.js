4071b49a10c40d4fca6f899180a886fc1a7cfe7e
Alex Bäuerle <alex.baeuerle@uni-ulm.de>
2019-11-11 14:57:40 +0100

    Made initial Working Version
    Added a favicon, links and some text to have an initial version of the
    page.

--- src/components/navigation/NavigationElements.js
+++ src/components/navigation/NavigationElements.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Grid, Link } from '@material-ui/core';
-import GitHub from '@material-ui/icons/GitHub';
+import { School, GitHub, Twitter, Language } from '@material-ui/icons';
 
 
 class NavigationElements extends React.Component {
@@ -13,6 +13,16 @@ class NavigationElements extends React.Component {
           justify="center"
           alignItems="center">
         <Grid item>
+          <Link href="https://scholar.google.com/citations?user=131euyAAAAAJ&hl=en&oi=ao">
+            <School
+                fontSize="inherit"
+                style={{
+                  width: 30,
+                  height: 30
+                }} />
+          </Link>
+        </Grid>
+        <Grid item>
           <Link href="https://www.github.com/sparkier">
             <GitHub
                 fontSize="inherit"
@@ -23,6 +33,24 @@ class NavigationElements extends React.Component {
           </Link>
         </Grid>
         <Grid item>
+          <Link href="https://www.twitter.com/a13xba">
+            <Twitter
+                fontSize="inherit"
+                style={{
+                  width: 30,
+                  height: 30
+                }} />
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link href="http://uni-ulm.de/?alex-baeuerle">
+            <Language
+                fontSize="inherit"
+                style={{
+                  width: 30,
+                  height: 30
+                }} />
+          </Link>
         </Grid>
       </Grid>
     );
