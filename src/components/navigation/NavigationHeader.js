a798af858e35e85a927bbeb5cf909179e1efc01c
Alex <alex.baeuerle@uni-ulm.de>
2019-11-12 19:24:07 +0100

    Images Changed
    Changed the favicon to have a transparent background and the avatar
    image. Also added a border to the avatar image.

--- src/components/navigation/NavigationHeader.js
+++ src/components/navigation/NavigationHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Avatar } from '@material-ui/core';
+import { Grid, Avatar, Box } from '@material-ui/core';
 import NavigationElements from './NavigationElements';
 
 class NavigationHeader extends React.Component {
@@ -13,13 +13,22 @@ class NavigationHeader extends React.Component {
           alignItems="center"
           className="navigationHead">
         <Grid item>
-          <Avatar
-              alt="Alex Bäuerle"
-              src={process.env.PUBLIC_URL + "alex.jpg"}
-              style={{
-                width: 150,
-                height: 150
-              }} />
+          <Box
+            borderRadius="50%"
+            borderColor="#000000"
+            border={0.5}
+            style={{
+              width: 151,
+              height: 151
+            }}>
+              <Avatar
+                alt="Alex Bäuerle"
+                src={process.env.PUBLIC_URL + "alex.jpg"}
+                style={{
+                  width: 150,
+                  height: 150
+                }} />
+          </Box>
         </Grid>
         <Grid item>
           <NavigationElements/>
