d20c6aad3a69efd9d3fb68e21768ca11a07450a5
Alex <alex.baeuerle@uni-ulm.de>
2020-09-29 12:00:30 +0200

    Removed Uni Link
    Removed the link to the university homeage as this is always broken and
    I dont want to be wasting more time on Typo3.

--- src/components/ContactElements.vue
+++ src/components/ContactElements.vue
@@ -1,9 +1,13 @@
 <template>
-  <v-row justify='center'>
+  <v-row justify="center">
     <v-btn icon href="mailto:alex.baeuerle@uni-ulm.de">
       <v-icon color="blue">mdi-email</v-icon>
     </v-btn>
-    <v-btn icon href="https://scholar.google.com/citations?user=131euyAAAAAJ&hl=en&oi=ao" target="_blank">
+    <v-btn
+      icon
+      href="https://scholar.google.com/citations?user=131euyAAAAAJ&hl=en&oi=ao"
+      target="_blank"
+    >
       <v-icon color="blue">mdi-school</v-icon>
     </v-btn>
     <v-btn icon href="https://www.github.com/sparkier" target="_blank">
@@ -12,17 +16,13 @@
     <v-btn icon href="https://www.twitter.com/a13xba" target="_blank">
       <v-icon color="blue">mdi-twitter</v-icon>
     </v-btn>
-    <v-btn icon href="http://uni-ulm.de/?alex-baeuerle" target="_blank">
-      <v-icon color="blue">mdi-web</v-icon>
-    </v-btn>
   </v-row>
 </template>
 
 <script>
 export default {
-  name: 'ContactElements',
+  name: "ContactElements",
 
-  data: () => ({
-  }),
+  data: () => ({}),
 };
 </script>
