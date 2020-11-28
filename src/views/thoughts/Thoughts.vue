f4e2f49550d6375fde999973307c19ead10a3303
Alex <alex.baeuerle@uni-ulm.de>
2020-11-28 11:30:18 +0100

    Thoughts Description Added

--- src/views/thoughts/Thoughts.vue
+++ src/views/thoughts/Thoughts.vue
@@ -1,12 +1,20 @@
 <template>
-  <v-col class="text-left">
-    <ThoughtAbstract
-      v-for="(thought, index) in thoughts"
-      :key="index"
-      :thought="thought"
-      :id="index"
-    />
-  </v-col>
+    <v-col class="text-left">
+      <v-container>
+        Thoughts are just what the name said, things I am thinking about.
+        If I am thinkning about something a lot, there is a chance I write these
+        thoughts down here, sometimes in just a few words, sometimes in a few
+        more, never super polished. If you agree with one of them, just want to
+        discuss, but especially if you disagree, please contact me, so we can
+        share our thoughts.
+      </v-container>
+      <ThoughtAbstract
+        v-for="(thought, index) in thoughts"
+        :key="index"
+        :thought="thought"
+        :id="index"
+      />
+    </v-col>
 </template>
 
 <script>
