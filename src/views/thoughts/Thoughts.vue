9f9d0076b0e99da44b9f4e7cfae27e3bf7f21d86
Alex <alex.baeuerle@uni-ulm.de>
2021-02-11 14:29:14 +0100

    New Thought

--- src/views/thoughts/Thoughts.vue
+++ src/views/thoughts/Thoughts.vue
@@ -4,9 +4,13 @@
         Thoughts are just what the name said, things I am thinking about.
         If I am thinkning about something a lot, there is a chance I write these
         thoughts down here, sometimes in just a few words, sometimes in a few
-        more, never super polished. If you agree with one of them, just want to
+        more, never polished. If you agree with one of them, just want to
         discuss, but especially if you disagree, please contact me, so we can
         share our thoughts.
+        <br/>
+        <br/>
+        Disclaimer: Most of this is not based on much research, and a lot is not
+        even proofread. This is just a loose summary of what I think about.
       </v-container>
       <ThoughtAbstract
         v-for="(thought, index) in thoughts"
