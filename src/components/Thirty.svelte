b048e5c828ba4633c2645e39ab667ac708faa768
Alex Bäuerle <alex@a13x.io>
2023-12-03 17:34:07 +0100

    details

--- src/components/Thirty.svelte
+++ src/components/Thirty.svelte
@@ -9,14 +9,15 @@
   </div>
   <div class="thirty-content-container">
     <h2>Wann</h2>
-    <p>Samstag, 09.12., Abends</p>
-    <p>Uhrzeit folgt.</p>
+    <p>Samstag, 09.12.</p>
+    <p>Start 18:00-19:00 Uhr, Ende offen.</p>
   </div>
   <div class="thirty-content-container">
     <h2>Wo</h2>
     <p>Rohmbachstr. 36, Oberteuringen</p>
     <p>Es wird Möglichkeiten zum Übernachten geben.</p>
-    <p>Bitte gebt mir Bescheid ob ihr kommen könnt.</p>
+    <p>Bitte gebt mir Bescheid ob ihr kommen könnt und Übernachten wollt.</p>
+    <p>Wir werden teilweise draußen sein, nehmt was warmes mit.</p>
   </div>
   <div class="thirty-content-container">
     <h2>Freu mich &#128525;</h2>
